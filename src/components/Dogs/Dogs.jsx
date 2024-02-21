414b81c178dfdb9f944bdb5f134c714d8459c95f
samadamp <samadampankhurst@gmail.comgit config --global user.name samadampgit statusgit config --global user.email samadampankhurst@gmail.com>
2024-02-21 16:13:03 +0100

    add images

--- src/components/Dogs/Dogs.jsx
+++ src/components/Dogs/Dogs.jsx
@@ -5,7 +5,9 @@ const Dogs = ({name, age, Neutered, image}) =>{
     
 return (<>
     <div>DOG:</div>
-    <p>NAME: {name}<br /> AGE: {age}  <br />NEUTERED STATUS:{Neutered} {image}</p>
+    <p>NAME: {name}<br /> AGE: {age}  <br />NEUTERED STATUS:{Neutered}</p>
+    <img src={image} alt={name} />
+
 
 
 </>)
